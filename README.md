5eba3fe96f59576daff1a16eab8059b474d7715e
gabrielgordbegli <ggordbegli@gmail.com>
2022-04-10 14:13:37 -0400

    append ratings

---
+++ README.md
@@ -0,0 +1,22 @@
+# Moodle Auto Login
+
+A simple Chrome extension that clicks the login button when you load Moonami.
+
+# How to add extension to Chrome
+
+## Chrome Webstore
+Moodle Auto Login is now available through the Chrome Web Store!
+
+https://chrome.google.com/webstore/detail/moodle-auto-login/nfpmdpogbdlfbniimbdkokpcmmleemia
+
+## Manual Installation
+_Click the image below for a 1 minute video that shows the installation process._
+[![A youtube video with installation instructions](https://img.youtube.com/vi/TmPhPxo_RtY/0.jpg)](https://www.youtube.com/watch?v=TmPhPxo_RtY)
+
+1. Download the repo as a zip file by clicking the green code button, then "Download Zip".
+2. Unzip the file into a folder.
+3. Go to the chrome extensions page, through Menu --> More Tools --> Extensions.
+4. Enable 'Developer Mode' on the top right of the page.
+5. Click 'Load unpacked' at the top left of the page.
+6. Select the folder from step 2.
+7. Enjoy!
---
+++ background.js
@@ -0,0 +1,52 @@
+function reddenPage() {
+  //gets all the professor names
+  let links = document.querySelectorAll('[href*="mailto:"]');
+  let url = 'http://localhost:3000/rmp-data/rmp-data.json';
+
+  fetch(url).then(res => res.json()).then(
+    function(container){
+      let data = container["data"];
+      alert(data);
+        //iterate through all links
+        for(let i = 0; i < links.length; i++) {
+          let currentLink_text = links[i].innerHTML.toString();
+          let namesArray = currentLink_text.trim().split(" ");
+          let trimmedNamesArray = namesArray.filter(function(entry) { return entry.trim() != ''; });
+
+          //if the professor's name is irregular, ignore it, we can add this functionaility in later
+          if(trimmedNamesArray.length !== 2){
+            continue;
+          }
+
+          let firstName = trimmedNamesArray[0];
+          let lastName = trimmedNamesArray[1];
+
+          //find the professor in the array of objects 'data' for which 'tFName' = firstName and 'tLName' = lastName
+          function getProfRating(){
+            for(var k = 0; k < data.length; k++){
+              if((data[k].tFname == firstName) && (data[k].tLname == lastName)){
+                return data[k].overall_rating;
+              }
+            }
+          }
+          
+          let profRating  = getProfRating();
+
+
+          links[i].innerHTML += ` <span style="color:red">${profRating}</span>`;
+        }
+
+    }
+  );
+}
+
+
+  
+chrome.action.onClicked.addListener((tab) => {
+  if(!tab.url.includes("chrome://")) {
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      function: reddenPage
+    });
+  }
+});
